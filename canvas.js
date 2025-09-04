a8d3d9946f4f8411cdf7de51d5b034e5baf41be0
Anjali Gupta <168294017+anjali-me@users.noreply.github.com>
2025-09-04 23:40:48 +0530

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,139 @@
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let pencilColor  = document.querySelectorAll(".pencil-color");
+let pencilWidthElem  = document.querySelector(".pencil-width");
+let eraserWidthElem  = document.querySelector(".eraser-width");
+let download = document.querySelector(".download");
+
+let undo = document.querySelector(".undo");
+let redo = document.querySelector(".redo");
+
+let penColor = "red";
+let eraserColor = "White";
+let penWidth = pencilWidthElem.value;
+let eraserWidth = eraserWidthElem.value;
+let mouseDown = false;
+
+let undoRedoTracker = [];
+let track = 0;
+
+let tool = canvas.getContext('2d');
+tool.strokeStyle = penColor;
+tool.lineWidth = penWidth;
+
+function beginPath(eventObj)
+{
+    tool.beginPath();
+    tool.moveTo(eventObj.x,eventObj.y);
+}
+
+function drawStroke(eventObj)
+{
+    tool.strokeStyle = eventObj.color;
+    tool.lineWidth = eventObj.width;
+    tool.lineTo(eventObj.x,eventObj.y);
+    tool.stroke();
+}
+
+canvas.addEventListener("mousedown",function(e){
+    mouseDown = true;
+    beginPath({
+        x:e.clientX,
+        y:e.clientY
+    });
+});
+
+canvas.addEventListener("mousemove",function(e){
+    if(mouseDown)
+    {
+        drawStroke({
+            x:e.clientX,
+            y:e.clientY,
+            color: eraserFlag ? eraserColor : penColor,
+            width: eraserFlag ? eraserWidth : penWidth
+        });
+    }
+});
+
+canvas.addEventListener("mouseup",function(e){
+    mouseDown = false;
+    let url = canvas.toDataURL();
+    undoRedoTracker.push(url);
+    track = undoRedoTracker.length - 1;
+});
+
+undo.addEventListener("click",function(e)
+{
+    if(track > 0)
+        track--;
+    let trackObj = {
+        trackValue: track,
+        undoRedoTracker
+    }
+    undoRedoTrack(trackObj);
+});
+
+redo.addEventListener("click",function(e)
+{
+    if(track < undoRedoTracker.length - 1)
+        track++;
+    let trackObj = {
+        trackValue: track,
+        undoRedoTracker
+    }
+    undoRedoTrack(trackObj);
+});
+
+function undoRedoTrack(trackObj)
+{
+    track = trackObj.trackValue;
+    undoRedoTracker = trackObj.undoRedoTracker;
+    let url = undoRedoTracker[track];
+    let img = new Image();
+    tool.clearRect(0, 0, canvas.width, canvas.height);  //Clears previous drawings
+    img.src = url;
+    img.onload = (e) => {
+        tool.drawImage(img,0,0,canvas.width,canvas.height);
+    }
+}
+
+pencilColor.forEach((colorElem) => {
+    colorElem.addEventListener("click", (e) => {
+        let color = colorElem.classList[0]; 
+        penColor = color;
+        tool.strokeStyle = penColor;
+    });
+});
+
+pencilWidthElem.addEventListener("change",function(e)
+{
+    penWidth = pencilWidthElem.value;
+    tool.lineWidth = penWidth;
+});
+
+eraserWidthElem.addEventListener("change",function(e)
+{
+    eraserWidth = eraserWidthElem.value;
+    tool.lineWidth = eraserWidth;
+});
+
+eraser.addEventListener("click", function(e){
+    if (eraserFlag) {
+        tool.strokeStyle = eraserColor;
+        tool.lineWidth = eraserWidth;
+    } else {
+        tool.strokeStyle = penColor;
+        tool.lineWidth = penWidth;
+    }
+});
+
+download.addEventListener("click",function(e)
+{
+    let url = canvas.toDataURL();
+    let a = document.createElement("a");
+    a.href = url;
+    a.download = "board.jpg";
+    a.click();
+});
---
+++ tools.js
@@ -0,0 +1,148 @@
+let optionsCont = document.querySelector(".options-cont");
+let toolsCont = document.querySelector(".tools-cont");
+let penciltoolCont = document.querySelector(".pencil-tool-cont");
+let erasertoolCont = document.querySelector(".eraser-tool-cont");
+let pencil = document.querySelector(".pencil");
+let eraser = document.querySelector(".eraser");
+let sticky = document.querySelector(".sticky");
+let upload = document.querySelector(".upload");
+
+let pencilFlag = false;
+let eraserFlag = false;
+let optionsFlag = true;
+
+//Tools toggling Logic
+optionsCont.addEventListener("click", (e) => {
+  optionsFlag = !optionsFlag;
+  if (optionsFlag) openTools();
+  else closeTools();
+});
+
+//To open the tools
+function openTools() {
+  let iconELement = optionsCont.children[0];
+  iconELement.classList.remove("fa-times");
+  iconELement.classList.add("fa-bars");
+  toolsCont.style.display = "flex";
+}
+
+//To close the tools
+function closeTools() {
+  let iconELement = optionsCont.children[0];
+  iconELement.classList.remove("fa-bars");
+  iconELement.classList.add("fa-times");
+  toolsCont.style.display = "none";
+  penciltoolCont.style.display = "none";
+  erasertoolCont.style.display = "none";
+}
+
+pencil.addEventListener("click", function (e) {
+  pencilFlag = !pencilFlag;
+  if (pencilFlag) penciltoolCont.style.display = "block";
+  else penciltoolCont.style.display = "none";
+});
+
+eraser.addEventListener("click", function (e) {
+  eraserFlag = !eraserFlag;
+  if (eraserFlag) erasertoolCont.style.display = "flex";
+  else erasertoolCont.style.display = "none";
+});
+
+upload.addEventListener("click", (e) => {
+    let input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.click();
+
+    input.addEventListener("change", (e) => {
+        let file = input.files[0];
+        let url = URL.createObjectURL(file);
+
+        let stickyTemplateHTML = `
+        <div class="header-cont">
+            <div class="minimize"></div>
+            <div class="remove"></div>
+        </div>
+        <div class="note-cont">
+            <img class="image-input" src="${url}"/>
+        </div>
+        `;
+        createSticky(stickyTemplateHTML);
+    })
+})
+
+sticky.addEventListener("click", (e) => {
+    let stickyTemplateHTML = `
+    <div class="header-cont">
+        <div class="minimize"></div>
+        <div class="remove"></div>
+    </div>
+    <div class="note-cont">
+        <textarea spellcheck="false"></textarea>
+    </div>
+    `;
+    createSticky(stickyTemplateHTML);
+})
+
+function createSticky(stickyTemplateHTML) {
+    let stickyCont = document.createElement("div");
+    stickyCont.setAttribute("class", "sticky-cont");
+    stickyCont.innerHTML = stickyTemplateHTML;
+    document.body.appendChild(stickyCont);
+
+    let minimize = stickyCont.querySelector(".minimize");
+    let remove = stickyCont.querySelector(".remove");
+    noteActions(minimize, remove, stickyCont);
+
+    stickyCont.onmousedown = function (event) {
+        dragAndDrop(stickyCont, event);
+    };
+
+    stickyCont.ondragstart = function () {
+        return false;
+    };
+}
+
+//Functions of sticky note
+function noteActions(minimize, remove, stickyCont) {
+  remove.addEventListener("click", function (e) {
+    stickyCont.remove();
+  });
+  minimize.addEventListener("click", function (e) {
+    let noteCont = stickyCont.querySelector(".note-cont");
+    let display = getComputedStyle(noteCont).getPropertyValue("display");
+    if (display === "none") {
+      noteCont.style.display = "block";
+    } else {
+      noteCont.style.display = "none";
+    }
+  });
+}
+
+//Took help from internet
+function dragAndDrop(element, event) {
+    let shiftX = event.clientX - element.getBoundingClientRect().left;
+    let shiftY = event.clientY - element.getBoundingClientRect().top;
+
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
+    moveAt(event.pageX, event.pageY);
+
+    // moves the ball at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+        element.style.left = pageX - shiftX + 'px';
+        element.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+    // move the ball on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+
+    // drop the ball, remove unneeded handlers
+    element.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        element.onmouseup = null;
+    };
+}
